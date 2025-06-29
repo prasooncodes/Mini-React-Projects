17b30181a62b31fb4918ebf114e7e6fcd9806282
Prasoon <prasoonmishra9936@gmail.com>
2025-06-29 14:22:46 +0530

    refactor: update dependencies and improve styling
    - Downgraded autoprefixer to version 10.4.14 for compatibility.
    - Downgraded postcss to version 8.4.31.
    - Updated tailwindcss to version 3.3.0.
    - Removed postcss.config.js and added postcss.config.cjs for better configuration handling.
    - Enhanced App component styling with a new layout and background color.

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -8,7 +8,11 @@ function App() {
 
   return (
     <>
-      <h1 className='text-2xl '>Password Generator</h1>
+      <div className="w-full h-screen bg-black">
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-white">
+        <h1 className='text-2xl text-center text-white my-3'>Password Generator</h1>
+      </div>
+    </div>
     </>
   )
 }
