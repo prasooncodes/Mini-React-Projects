d97f2e9c6c87f2689f0086e8fe7f697ae798e555
Prasoon <prasoonmishra9936@gmail.com>
2025-07-02 18:56:51 +0530

    feat: implement currency conversion functionality with InputBox component and useCurrencyInfo hook

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {InputBox} from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+  
 
   return (
     <>
-      <h1 className='text-3xl text-center text'></h1>
+      <h1 className='text-3xl text-center text-white bg-gray-700'>Currency App</h1>
     </>
   )
 }
