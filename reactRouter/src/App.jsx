5302de8f2089563553f96e76c907f26b7bbffe25
Prasoon <prasoonmishra9936@gmail.com>
2025-07-05 16:39:13 +0530

    feat: add User component and route for user profile

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -19,6 +19,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/user/:id" element={<User />} />
       </Routes>
       <Footer />
     </BrowserRouter>
